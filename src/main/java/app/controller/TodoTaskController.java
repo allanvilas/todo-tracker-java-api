1c38ad4d9acf06a2d2e445a4748303a4c888bd77
allanvilas <allanvilasdeveloper@gmail.com>
2024-08-29 03:28:01 -0300

    added swagger OpenAPI config to the project

--- src/main/java/app/controller/TodoTaskController.java
+++ src/main/java/app/controller/TodoTaskController.java
@@ -1,7 +1,5 @@
 package app.controller;
 
-import java.util.HashMap;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +17,11 @@ import app.dto.CompleteTodoTaskDTO;
 import app.dto.NewTaskDTO;
 import app.model.TodoTask;
 import app.service.TodoTaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 
 @RestController
@@ -52,6 +55,14 @@ public class TodoTaskController {
         return todoTaskService.updateTaskFlag(DTO);
     }
 
+
+    @Operation(summary = "Create a new task")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Task created",
+                content = { @Content(mediaType = "application/json",
+                schema = @Schema(implementation = TodoTask.class)) }),
+        @ApiResponse(responseCode = "400", description = "Invalid input", content = @Content),
+        @ApiResponse(responseCode = "500", description = "Server error", content = @Content) })
     @PostMapping
     private ResponseEntity<String> createNewTask(@Valid @RequestBody NewTaskDTO DTO,
             UriComponentsBuilder ucb) {
